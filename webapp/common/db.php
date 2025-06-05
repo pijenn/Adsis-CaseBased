6892032b769395936d8872ae053dd0557ed23873
hanzhone <johanarizona@gmail.com>
2025-06-06 03:10:22 +0700

    Update koneksi db

--- webapp/common/db.php
+++ webapp/common/db.php
@@ -1,10 +1,9 @@
 <?php
-$host = 'localhost';
+$host = 'db';
 $db   = 'ujian';
-$user = 'root';
-$pass = '';
+$user = 'admin';
+$pass = 'admin123';
 $charset = 'utf8mb4';
-date_default_timezone_set('Asia/Jakarta');
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
